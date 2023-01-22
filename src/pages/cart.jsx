f26612e6c387936f56c6f5f4b72f2208201921dd
CristiCMC <100723697+CristiCMC@users.noreply.github.com>
2023-01-22 17:50:00 +0000

    curs 18.01.2023

---
+++ src/pages/cart.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+
+const CartPage = () => {
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+    </>
+  );
+};
+
+
+export default CartPage;
