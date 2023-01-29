d0b057eb192e05d070868da24528bfc907795810
CristiCMC <100723697+CristiCMC@users.noreply.github.com>
2023-01-29 19:21:47 +0000

    curs 25.01.2023

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,52 +4,50 @@ import { CgMenu } from 'react-icons/cg';
 
 export const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
-  const jsxVar =  <span className="bg-white block w-4 h-4 rounded-full"></span>;
+  const jsxVar = <span className="bg-white block w-4 h-4 rounded-full"></span>;
 
   return (
-  <>
-    <section className="flex lg:flex-col justify-between items-center h-full lg:py-4 z-10 relative">
-      <Link href="/">
-        {/* {not use for anchor} */}
-        {jsxVar}
-      </Link>
+    <>
+      <section className="bg-neutral-900 flex lg:flex-col justify-between items-center h-full lg:py-4 z-10  relative">
+        <Link href="/">
+          {/* no use for anchor */}
+          {jsxVar}
+        </Link>
 
-      <button
-        title="Menu"
-        type="button"
-        onClick={() => {
-          setMenuOpen(!menuOpen);
-        }}
+        <button
+          title="Menu"
+          type="button"
+          onClick={() => {
+            setMenuOpen(!menuOpen);
+          }}
+        >
+          <CgMenu size="32"></CgMenu>
+        </button>
+
+        <ul className="hidden lg:block">
+          <li>{jsxVar}</li>
+        </ul>
+      </section>
+
+      <nav
+        className={`z-0 absolute left-0 -top-full ${
+          menuOpen ? 'translate-y-full' : ''
+        } transition-transform transform-gpu w-screen h-screen bg-neutral-900 lg:w-screen-1/3`}
       >
-        <CgMenu size="32"></CgMenu>
-      </button>
+        <ul>
+          <li>
+            <Link href="/" title="Home">
+              Home
+            </Link>
+          </li>
 
-      <ul className="hidden lg:block">
-        <li>{jsxVar}</li>
-      </ul>
-    </section>
-
-    <nav
-      className={`absolute left-0 -top-full ${
-        menuOpen ? 'translate-y-full' : ''
-      } transition-transform transform-gpu w-screen h-screen lg:w-screen-1/3 bg-neutral-900`}
-      >
-      <ul>
-        <li>
-          <Link href="/" title="Home">
-            Home
-          </Link>
-        </li>
-      </ul>
-
-      <ul>
-        <li>
-          <Link href="/" title="Contact">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </>
+          <li>
+            <Link href="/" title="Contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
